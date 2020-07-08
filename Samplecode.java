6b20774fdf4b3bbe940c9896a7beb3d7ccdda107
jacblr43 <63628685+jacblr43@users.noreply.github.com>
2020-07-08 23:48:20 +0530

    Update Samplecode.java

--- Samplecode.java
+++ Samplecode.java
@@ -3,6 +3,6 @@ class Sample
 public static void main(String args[]) throws InterruptedException
                 {
 Thread.sleep(10000);
-System.out.println("Hello Java");
+System.out.println("Hello Java -- this is a test");
                 }
 }
